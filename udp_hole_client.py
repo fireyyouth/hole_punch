705326c3685462096e1d4b0c7dc39e1c01ca6628
fireyyouth <fireyyouth@gmail.com>
2020-11-01 11:07:38 +0000

    chore: handle exception better

--- udp_hole_client.py
+++ udp_hole_client.py
@@ -2,6 +2,8 @@ import socket
 from threading import Thread
 import time
 import requests
+import os
+import traceback
 
 REMOTE_ADDR = ('34.84.245.193', 9999)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,22 +11,47 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('0.0.0.0', 12345))
 
 def heartbeat():
-    while True:
-        s.sendto(b'!', REMOTE_ADDR)
-        time.sleep(1)
+    global s
+    try:
+        while True:
+            s.sendto(b'!', REMOTE_ADDR)
+            time.sleep(1)
+    except:
+        traceback.print_exc()
+        os._exit(1)
 
 def greet():
-    while True:
-        rsp = requests.get('http://{}:{}'.format(*REMOTE_ADDR))
-        for item in rsp.json():
-            peer = (item['ip'], item['port'])
-            s.sendto(b'hello', peer)
-        time.sleep(1)
+    global s
+    try:
+        while True:
+            rsp = requests.get('http://{}:{}'.format(*REMOTE_ADDR))
+            for item in rsp.json():
+                peer = (item['ip'], item['port'])
+                s.sendto(b'hello', peer)
+            time.sleep(1)
+    except:
+        traceback.print_exc()
+        os._exit(1)
 
-back_jobs = [Thread(target=heartbeat), Thread(target=greet)]
+def receive():
+    global s
+    try:
+        while True:
+            data, addr = s.recvfrom(128)
+            print('received', data, addr)
+    except:
+        traceback.print_exc()
+        os._exit(1)
+
+back_jobs = [
+        Thread(target=heartbeat),
+        Thread(target=greet),
+        Thread(target=receive),
+        ]
+
 for job in back_jobs:
     job.start()
 
-while True:
-    data, addr = s.recvfrom(128)
-    print('received', data, addr)
+for job in back_jobs:
+    job.join()
+
--- udp_hole_server.py
+++ udp_hole_server.py
@@ -5,6 +5,8 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import collections
 import time
 import json
+import os
+import traceback
 
 HEARTBEAT_INTERVAL = 3
 
@@ -13,26 +15,31 @@ time_index = collections.OrderedDict()
 mutex = Lock()
 
 def clear_expired():
-    global time_index
-    while True:
-        now = time.time()
-        poped = False
-        with mutex:
-            while len(time_index):
-                t = next(iter(time_index))
-                if t >= now:
-                    break
-                for addr in time_index[t]:
-                    online.pop(addr)
-                time_index.pop(t)
-                poped = True
-        if poped:
-            print('back', online, time_index)
-        time.sleep(1)
+    global online, time_index, mutex
+    try:
+        while True:
+            now = time.time()
+            poped = False
+            with mutex:
+                while len(time_index):
+                    t = next(iter(time_index))
+                    if t >= now:
+                        break
+                    for addr in time_index[t]:
+                        online.pop(addr)
+                    time_index.pop(t)
+                    poped = True
+            if poped:
+                print('clear_expired():', online, time_index)
+            time.sleep(1)
+    except:
+        traceback.print_exc()
+        os._exit(1)
 
 
 class MyHTTPHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        global online
         self.send_response(HTTPStatus.OK)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
@@ -42,37 +49,51 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(r).encode())
 
 def httpd():
-    HTTPServer(('', 9999), MyHTTPHandler).serve_forever()
+    try:
+        HTTPServer(('', 9999), MyHTTPHandler).serve_forever()
+    except:
+        traceback.print_exc()
+        os._exit(1)
 
+
+
+def track():
+    global online, time_index, mutex
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.bind(('0.0.0.0', 9999))
+        while True:
+            data, addr = s.recvfrom(128)
+
+            new_time = time.time() + HEARTBEAT_INTERVAL
+
+            with mutex:
+                if addr in online:
+                    old_time = online[addr]
+                    time_index[old_time].remove(addr)
+                    if len(time_index[old_time]) == 0:
+                        time_index.pop(old_time)
+
+                online[addr] = new_time
+
+                if new_time in time_index:
+                    time_index[new_time].add(addr)
+                else:
+                    time_index[new_time] = {addr}
+
+            print('track():', online, time_index)
+    except:
+        traceback.print_exc()
+        os._exit(1)
 
 back_jobs = [
         Thread(target=clear_expired),
         Thread(target=httpd),
+        Thread(target=track),
 ]
 
 for job in back_jobs:
     job.start()
 
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('0.0.0.0', 9999))
-while True:
-    data, addr = s.recvfrom(128)
-
-    new_time = time.time() + HEARTBEAT_INTERVAL
-
-    with mutex:
-        if addr in online:
-            old_time = online[addr]
-            time_index[old_time].remove(addr)
-            if len(time_index[old_time]) == 0:
-                time_index.pop(old_time)
-
-        online[addr] = new_time
-
-        if new_time in time_index:
-            time_index[new_time].add(addr)
-        else:
-            time_index[new_time] = {addr}
-
-    print('main', online, time_index)
+for job in back_jobs:
+    job.join()
